75d2667d3da70ad7c383a29624ae796887624ac2
Mark Poscablo <mark@arrayfire.com>
2019-06-30 22:59:14 -0500

    [WIP] Change name to AFLogisticRegression, emulate SKBaggingClassifier example

---
+++ af_primitives/AFLogisticRegression.py
@@ -0,0 +1,263 @@
+from typing import Any, Callable, List, Dict, Union, Optional, Sequence, Tuple
+import typing
+from numpy import ndarray
+import numpy as np
+from collections import OrderedDict
+from scipy import sparse
+import os
+
+import arrayfire as af
+from arrayfire.algorithm import max, imax, count, sum
+from arrayfire.array import read_array, transpose
+from arrayfire.arith import abs, sigmoid, log
+from arrayfire.blas import matmul, matmulTN
+from arrayfire.data import constant, join, moddims
+from arrayfire.device import sync, eval
+from arrayfire.interop import from_ndarray
+
+from d3m.container.numpy import ndarray as d3m_ndarray
+from d3m.container import DataFrame as d3m_dataframe
+from d3m.metadata import hyperparams, params, base as metadata_base
+from d3m import utils
+from d3m.primitive_interfaces.base import CallResult, DockerContainer
+import common_primitives.utils as common_utils
+from d3m.primitive_interfaces.supervised_learning import SupervisedLearnerPrimitiveBase
+from d3m.primitive_interfaces.base import ProbabilisticCompositionalityMixin
+
+Inputs = Union[d3m_dataframe, d3m_ndarray]
+Outputs = d3m_ndarray
+
+class Params(params.Params):
+    n_classes_: Optional[int]
+    n_features_: Optional[int]
+
+
+class Hyperparams(hyperparams.Hyperparams):
+    learning_rate = hyperparams.Hyperparameter[float](
+        default=0.1,
+        description='(alpha) Rate at which to update the weights at each iteration during gradient descent',
+        semantic_types=['https://metadata.datadrivendiscovery.org/types/TuningParameter']
+    )
+    reg_constant = hyperparams.Hyperparameter[float](
+        default=1.0,
+        description='(lambda) Weight decay',
+        semantic_types=['https://metadata.datadrivendiscovery.org/types/TuningParameter']
+    )
+    max_err = hyperparams.Hyperparameter[float](
+        default=0.01,
+        description='Maximum error',
+        semantic_types=['https://metadata.datadrivendiscovery.org/types/TuningParameter']
+    )
+    max_iter = hyperparams.Bounded[int](
+        default=1000,
+        lower=0,
+        upper=None,
+        description='Maximum number of iterations taken for the solver to converge.',
+        semantic_types=['https://metadata.datadrivendiscovery.org/types/TuningParameter']
+    )
+    verbose = hyperparams.Hyperparameter[int](
+        default=0,
+        description="Controls the verbosity of the building process.",
+        semantic_types=['https://metadata.datadrivendiscovery.org/types/TuningParameter']
+    )
+
+
+class AFLogisticRegression(SupervisedLearnerPrimitiveBase[Inputs, Outputs, Params, Hyperparams],
+                           ProbabilisticCompositionalityMixin[Inputs, Outputs, Params, Hyperparams]):
+    """
+    Primitive implementing LogisticRegression using the ArrayFire library
+    """
+
+    __author__ = "ArrayFire"
+    metadata = metadata_base.PrimitiveMetadata({
+         "algorithm_types": [metadata_base.PrimitiveAlgorithmType.LOGISTIC_REGRESSION, ],
+         "name": "af.LogisticRegression",
+         "primitive_family": metadata_base.PrimitiveFamily.REGRESSION,
+         "python_path": "d3m.primitives.af_primitives.AFLogisticRegression",
+         #"source": {'name': 'ArrayFire', 'contact': 'mailto:support@arrayfire.com', 'uris': ['https://gitlab.com/arrayfire/arrayfire']},
+         "version": "0.0.1",
+         "id": "73dff093-f8fe-4e9e-a5af-88a7e4398a43"
+         #TODO:
+         # 'installation': [
+         #                {'type': metadata_base.PrimitiveInstallationType.PIP,
+         #                   'package_uri': 'git+https://gitlab.com/datadrivendiscovery/sklearn-wrap.git@{git_commit}#egg=sklearn_wrap'.format(
+         #                       git_commit=utils.current_git_commit(os.path.dirname(__file__)),
+         #                    ),
+         #                }]
+    })
+
+    def __init__(self, *,
+                 hyperparams: Hyperparams,
+                 random_seed: int = 0,) -> None:
+
+        super().__init__(hyperparams=hyperparams, random_seed=random_seed)
+
+        self._alpha = self.hyperparams['learning_rate']
+        self._lambda_param = self.hyperparams['reg_constant']
+        self._maxerr = self.hyperparams['max_err']
+        self._maxiter = self.hyperparams['max_iter']
+        self._verbose = bool(self.hyperparams['verbose'])
+
+        self._training_inputs = None
+        self._training_outputs = None
+        self._fitted = False
+        self._weights = None
+
+        self._classes = None
+        self._n_classes = 0
+        self._n_features = 0
+
+    def set_training_data(self, *, inputs: Inputs, outputs: Outputs) -> None:
+        self._training_inputs = inputs
+        self._training_outputs = outputs
+        self._fitted = False
+
+    def _accuracy(self, predicted, target):
+        _, tlabels = af.imax(target, 1)
+        _, plabels = af.imax(predicted, 1)
+        return 100 * af.count(plabels == tlabels) / tlabels.elements()
+
+    def _abserr(self, predicted, target):
+        return 100 * af.sum(af.abs(predicted - target)) / predicted.elements()
+
+    def _predict_proba(self, X, Weights):
+        Z = af.matmul(X, Weights)
+        return af.sigmoid(Z)
+
+    def _predict_log_proba(self, X, Weights):
+        return af.log(self._predict_proba(X, Weights))
+
+    def _predict(self, X, Weights):
+        probs = self._predict_proba(X, Weights)
+        _, classes = af.imax(probs, 1)
+        return classes
+
+    def _cost(self, Weights, X, Y, lambda_param=1.0):
+        # Number of samples
+        m = Y.dims()[0]
+
+        dim0 = Weights.dims()[0]
+        dim1 = Weights.dims()[1] if len(Weights.dims()) > 1 else None
+        dim2 = Weights.dims()[2] if len(Weights.dims()) > 2 else None
+        dim3 = Weights.dims()[3] if len(Weights.dims()) > 3 else None
+        # Make the lambda corresponding to Weights(0) == 0
+        lambdat = af.constant(lambda_param, dim0, dim1, dim2, dim3)
+
+        # No regularization for bias weights
+        lambdat[0, :] = 0
+
+        # Get the prediction
+        H = self._predict_proba(X, Weights)
+
+        # Cost of misprediction
+        Jerr = -1 * af.sum(Y * af.log(H) + (1 - Y) * af.log(1 - H), dim=0)
+
+        # Regularization cost
+        penalty_norm = None
+        if self.hyperparams['penalty'] == 'l2':
+            penalty_norm = Weights * Weights
+        else:
+            penalty_norm = af.abs(Weights)
+        Jreg = 0.5 * af.sum(lambdat * penalty_norm, dim=0)
+
+        # Total cost
+        J = (Jerr + Jreg) / m
+
+        # Find the gradient of cost
+        D = (H - Y)
+        dJ = (af.matmulTN(X, D) + lambdat * Weights) / m
+
+        return J, dJ
+
+    def _ints_to_onehots(self, digits, num_classes):
+        onehots = np.zeros((digits.shape[0], num_classes), dtype='float32')
+        onehots[np.arange(digits.shape[0]), digits] = 1
+        return onehots
+
+    def _train(self, X, Y, alpha=0.1, lambda_param=1.0, maxerr=0.01, maxiter=1000):
+        # Initialize parameters to 0
+        Weights = af.constant(0, X.dims()[1], Y.dims()[1])
+
+        for i in range(maxiter):
+            # Get the cost and gradient
+            J, dJ = self._cost(Weights, X, Y, lambda_param)
+            err = af.max(af.abs(J))
+            if err < maxerr:
+                return Weights
+
+            # Update the weights via gradient descent
+            Weights = Weights - alpha * d
+
+        return Weights
+
+    def fit(self, *, timeout: float = None, iterations: int = None) -> CallResult[None]:
+        # Assume that inputs at this point are already ndarrays
+        if self._fitted:
+            return CallResult(None)
+
+        if self._training_inputs is None or self._training_outputs is None:
+            raise ValueError("Missing training data.")
+
+        # Assume training input data is an ndarray
+        training_inputs = self._training_inputs
+        training_outputs = self._training_outputs.values
+        params = self.get_params()
+
+        if self._n_classes == 0:
+            self._n_classes = np.amax(training_outputs[1] + 1)
+        self._n_features = training_inputs.shape[1]
+
+        # Flatten output if needed
+        shape = training_outputs.shape
+        if len(shape) == 2 and shape[1] == 1:
+            training_outputs = numpy.ravel(training_outputs)
+
+        # Convert ndarray to af array
+        train_images = af.from_ndarray(training_inputs)
+        train_targets = af.from_ndarray(
+            self._ints_to_onehots(training_outputs, self._n_classes)
+        )
+
+        # Flatten training samples if they're multidimensional
+        num_train = train_images.dims()[0]
+        feature_length = int(train_images.elements() / num_train);
+        train_feats = af.moddims(train_images, num_train, feature_length)
+
+        # Add bias
+        train_bias = af.constant(1, num_train, 1)
+        train_feats = af.join(1, train_bias, train_feats)
+
+        # Start training
+        self._weights = self._train(train_feats, train_targets,
+                                    self._alpha,
+                                    self._lambda_param,
+                                    self._maxerr,
+                                    self._maxiter
+        )
+
+        self._fitted = True
+
+        return CallResult(None)
+
+    def produce(self, *, inputs: Inputs, timeout: float = None, iterations: int = None) -> CallResult[Outputs]:
+        # We may have to adjust the dimensions when doing this conversion
+        af_inputs = af.from_ndarray(inputs.values)
+        af_output = self._predict(af_inputs, self._Weights)
+        output = af_output.to_ndarray()
+        return CallResult(d3m_ndarray(output))
+
+    def get_params(self) -> Params:
+        if not self._fitted:
+            raise ValueError("Fit not performed.")
+
+        return Params(
+            n_classes_ = self._n_classes,
+            n_features_ = self._n_features
+        )
+
+
+    def set_params(self, *, params: Params) -> None:
+        self._n_classes = params['n_classes_']
+        self._n_features = params['n_features_']
+
+
---
+++ af_primitives/test_AFLogisticRegression.py
@@ -0,0 +1,244 @@
+import unittest
+import numpy as np
+import pickle
+from typing import NamedTuple
+
+from sklearn import datasets
+from sklearn.utils import shuffle
+
+import AFLogisticRegression
+# from autogenerate.d3m_sklearn_wrap.sklearn_wrap import AFLogisticRegression
+
+# Taken from original af logit example
+import arrayfire as af
+from arrayfire.algorithm import count, imax, sum
+from arrayfire.arith import abs, log, sigmoid
+from arrayfire.blas import matmul, matmulTN
+from arrayfire.data import constant, join
+from arrayfire.device import eval, sync
+from arrayfire.interop import from_ndarray
+from sklearn.datasets import load_iris
+
+# Common random state
+rng = np.random.RandomState(0)
+
+# Toy sample from sklearn tests
+X = [[-2, -1], [-1, -1], [-1, -2], [1, 1], [1, 2], [2, 1]]
+y_class = ["foo", "foo", "foo", 1, 1, 1]    # test string class labels
+T = [[-1, -1], [2, 2], [3, 2]]
+y_t_class = ["foo", 1, 1]
+
+# Load the iris dataset and randomly permute it
+iris = datasets.load_iris()
+perm = rng.permutation(iris.target.size)
+iris.data, iris.target = shuffle(iris.data, iris.target, random_state=rng)
+
+Hyperparams = NamedTuple('Hyperparams', [
+    ('learning_rate', float),
+    ('reg_constant', float),
+    ('max_err', float),
+    ('max_iter', int),
+    ('verbose', int),
+])
+
+
+def ints_to_onehots(ints, num_classes):
+    onehots = np.zeros((ints.shape[0], num_classes), dtype='float32')
+    onehots[np.arange(ints.shape[0]), ints] = 1
+    return onehots
+
+
+def onehots_to_ints(onehots):
+    return np.argmax(onehots, axis=1)
+
+
+def read_and_preprocess_iris_data():
+    X, y = load_iris(return_X_y=True)
+    X = X.astype('float32')
+    y = y.astype('uint32')
+    return (X, y, X, y)
+
+
+def accuracy(predicted, target):
+    _, tlabels = af.imax(target, 1)
+    _, plabels = af.imax(predicted, 1)
+    return 100 * af.count(plabels == tlabels) / tlabels.elements()
+
+
+def abserr(predicted, target):
+    return 100 * af.sum(af.abs(predicted - target)) / predicted.elements()
+
+
+class RefAfLogisticRegression:
+    def __init__(self, alpha=0.1, lambda_param=1.0, maxerr=0.01, maxiter=1000, verbose=False):
+        self.__alpha = alpha
+        self.__lambda_param = lambda_param
+        self.__maxerr = maxerr
+        self.__maxiter = maxiter
+        self.__verbose = verbose
+        self.__weights = None
+
+
+    def predict_proba(self, X):
+        Z = af.matmul(X, self.__weights)
+        return af.sigmoid(Z)
+
+
+    def predict_log_proba(self, X):
+        return af.log(self.predict_proba(X))
+
+
+    def predict(self, X):
+        probs = self.predict_proba(X)
+        _, classes = af.imax(probs, 1)
+        return classes
+
+
+    def cost(self, X, Y):
+        # Number of samples
+        m = Y.dims()[0]
+
+        dim0 = self.__weights.dims()[0]
+        dim1 = self.__weights.dims()[1] if len(self.__weights.dims()) > 1 else None
+        dim2 = self.__weights.dims()[2] if len(self.__weights.dims()) > 2 else None
+        dim3 = self.__weights.dims()[3] if len(self.__weights.dims()) > 3 else None
+        # Make the lambda corresponding to self.__weights(0) == 0
+        lambdat = af.constant(self.__lambda_param, dim0, dim1, dim2, dim3)
+
+        # No regularization for bias weights
+        lambdat[0, :] = 0
+
+        # Get the prediction
+        H = self.predict_proba(X)
+
+        # Cost of misprediction
+        Jerr = -1 * af.sum(Y * af.log(H) + (1 - Y) * af.log(1 - H), dim=0)
+
+        # Regularization cost
+        Jreg = 0.5 * af.sum(lambdat * self.__weights * self.__weights, dim=0)
+
+        # Total cost
+        J = (Jerr + Jreg) / m
+
+        # Find the gradient of cost
+        D = (H - Y)
+        dJ = (af.matmulTN(X, D) + lambdat * self.__weights) / m
+
+        return J, dJ
+
+
+    def train(self, X, Y):
+        # Initialize parameters to 0
+        self.__weights = af.constant(0, X.dims()[1], Y.dims()[1])
+
+        for i in range(self.__maxiter):
+            # Get the cost and gradient
+            J, dJ = self.cost(X, Y)
+            err = af.max(af.abs(J))
+            if err < self.__maxerr:
+                print('Iteration {0:4d} Err: {1:4f}'.format(i + 1, err))
+                print('Training converged')
+                return self.__weights
+
+            if self.__verbose and ((i+1) % 10 == 0):
+                print('Iteration {0:4d} Err: {1:4f}'.format(i + 1, err))
+
+            # Update the weights via gradient descent
+            self.__weights = self.__weights - self.__alpha * dJ
+
+        if self.__verbose:
+            print('Training stopped after {0:d} iterations'.format(self.__maxiter))
+
+
+    def eval(self):
+        af.eval(self.__weights)
+        af.sync()
+
+
+
+class TestAFLogisticRegression(unittest.TestCase):
+
+    def test(self):
+        ############# Pure arrayfire-python example ###########
+
+        # Determine number of classes if not provided
+        dataset = read_and_preprocess_iris_data()
+        # train_feats = iris.data
+        # train_targets = iris.target
+        num_classes = np.amax(dataset[1] + 1)
+
+        # Convert numpy array to af array (and convert labels/targets from ints to
+        # one-hot encodings)
+        train_feats = af.from_ndarray(dataset[0])
+        train_targets = af.from_ndarray(ints_to_onehots(dataset[1], num_classes))
+        test_feats = af.from_ndarray(dataset[2])
+        test_targets = af.from_ndarray(ints_to_onehots(dataset[3], num_classes))
+
+        num_train = train_feats.dims()[0]
+        num_test = test_feats.dims()[0]
+
+        # Add bias
+        train_bias = af.constant(1, num_train, 1)
+        test_bias = af.constant(1, num_test, 1)
+        train_feats = af.join(1, train_bias, train_feats)
+        test_feats = af.join(1, test_bias, test_feats)
+
+        clf = RefAfLogisticRegression(alpha=0.1,          # learning rate
+                                      lambda_param = 1.0, # regularization constant
+                                      maxerr=0.01,        # max error
+                                      maxiter=1000,       # max iters
+                                      verbose=False       # verbose mode
+        )
+
+        clf.train(train_feats, train_targets)
+        test_outputs = clf.predict_proba(test_feats)
+
+        ############# d3m-arrayfire example ###########
+
+
+        # classes = np.unique(iris.target)
+        # hyperparams = AFLogisticRegression.Hyperparams.defaults()
+        # # Create the model object
+        # clf = AFLogisticRegression.AFLogisticRegression(hyperparams=hyperparams)
+        # train_set = iris.data
+        # targets = iris.target
+        # clf.set_training_data(inputs=train_set, outputs=targets)
+        # clf.fit()
+
+        # output = clf.produce(inputs=train_set)
+
+        # # Testing get_params() and set_params()
+        # params = clf.get_params()
+        # clf.set_params(params=params)
+        # first_output = clf.produce(inputs=train_set)
+
+        # # pickle the params and hyperparams
+        # pickled_params = pickle.dumps(params)
+        # unpickled_params = pickle.loads(pickled_params)
+
+        # pickled_hyperparams = pickle.dumps(hyperparams)
+        # unpickled_hyperparams = pickle.loads(pickled_hyperparams)
+
+        # # Create a new object from pickled params and hyperparams
+        # new_clf = AFLogisticRegression.AFLogisticRegression(hyperparams=unpickled_hyperparams)
+        # new_clf.set_params(params=unpickled_params)
+        # new_output = new_clf.produce(inputs=train_set)
+
+        # # Check if outputs match
+        # self.assertTrue(np.array_equal(first_output.value, output.value))
+        # self.assertTrue(np.array_equal(new_output.value, output.value))
+        # # We want to test the running of the code without errors and not the correctness of it
+        # # since that is assumed to be tested by sklearn
+        # # assert np.array_equal(classes, clf._clf.classes_)
+        # print("SUCCESS: Test fit produce on AFLogisticRegression")
+
+        # model = pickle.dumps(clf)
+        # new_clf = pickle.loads(model)
+        # new_output = new_clf.produce(inputs=train_set)
+        # self.assertTrue(np.array_equal(output.value, output.value))
+        # self.assertTrue(np.array_equal(new_output.value, output.value))
+        # print("SUCCESS: Test pickling entire model on AFLogisticRegression")
+
+
+if __name__ == '__main__':
+    unittest.main()
