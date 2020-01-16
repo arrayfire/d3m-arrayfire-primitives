8e2648e995c6d48bd08955c052a886b0425219da
Mark Poscablo <mark@arrayfire.com>
2020-01-16 16:44:37 -0600

    Add arrayfire installation step in setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,9 @@
 import os
 import re
 from setuptools import setup, find_packages
+from setuptools.command.develop import develop
+from setuptools.command.install import install
+from subprocess import check_call
 
 PACKAGE_NAME = 'af_primitives'
 
@@ -15,6 +18,24 @@ def read_package_variable(key):
     raise KeyError("'{0}' not found in '{1}'".format(key, module_path))
 
 
+def install_arrayfire():
+    check_call('mkdir -p /opt/arrayfire'.split())
+    check_call('wget -P /tmp https://arrayfire.s3.amazonaws.com/3.6.4/ArrayFire-v3.6.4_Linux_x86_64.sh'.split())
+    check_call('bash /tmp/ArrayFire-v3.6.4_Linux_x86_64.sh --skip-license --prefix=/opt/arrayfire'.split())
+
+
+class PostDevelopStep(develop):
+    def run(self):
+        install_arrayfire()
+        develop.run(self)
+
+
+class PostInstallStep(install):
+    def run(self):
+        install_arrayfire()
+        install.run(self)
+
+
 setup(
     name=PACKAGE_NAME,
     version=read_package_variable('__version__'),
@@ -25,12 +46,16 @@ setup(
         'arrayfire==3.6.20181017',
         'd3m==2019.11.10'
     ],
-    url='https://gitlab.com/mark-poscablo/d3m-arrayfire-primitives',
+    url='https://github.com/arrayfire/d3m-arrayfire-primitives',
     keywords='d3m_primitive',
     entry_points={
         'd3m.primitives': [
             'classification.logistic_regression.ArrayFire = af_primitives.af_LogisticRegression:af_LogisticRegression',
             'classification.k_neighbors.ArrayFire = af_primitives.af_KNeigborsClassifier:af_KNeighborsClassifier'
         ],
+    },
+    cmdclass={
+        'develop': PostDevelopStep,
+        'install': PostInstallStep,
     },
 )
